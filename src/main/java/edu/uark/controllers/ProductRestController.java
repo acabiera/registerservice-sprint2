21b531b113544bd73061aa779b53ee5874dea469
acabiera <acabiera@uark.edu>
2017-11-15 17:08:29 -0600

    Attempt 1: Formating the product listing to read cleaner on the url regserv site

--- src/main/java/edu/uark/controllers/ProductRestController.java
+++ src/main/java/edu/uark/controllers/ProductRestController.java
@@ -35,6 +35,7 @@ public class ProductRestController {
 
 	@RequestMapping(value = "/apiv0/products", method = RequestMethod.GET)
 	public ProductListing getProducts() {
+		System.out.println("\n");
 		return (new ProductsQuery()).execute();
 	}
 	
